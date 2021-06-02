d90197db8d2ce64ac840526d5d4266da52f34345
Joy Sureshkumar <>
2021-06-02 16:13:26 +0000

    change

--- script.js
+++ script.js
@@ -4,22 +4,22 @@ document.getElementById('hide').addEventListener('click', hide)
 document.getElementById('show').addEventListener('click', show)
 document.getElementById('bg').addEventListener('click', background)
 // when image button is clicked, it changes the img between the first lion picture and the second lion picture
-function image() {
+function image () {
   document.getElementById('picture').src = 'images/Albedo.jpeg'
 }
 // when text button is clicked, the text changes
-function text() {
+function text () {
   document.getElementById('text').innerHTML = 'New Text'
 }
 // when hide button is clicked, the text disapears
-function hide() {
+function hide () {
   document.getElementById('text').style.display = 'None'
 }
 // when show button is clicked, the hidden text will be revelaed again
-function show() {
+function show () {
   document.getElementById('text').style.display = 'block'
 }
 // when background is clicked, the color of the background changes 
-function background() {
+function background () {
   document.body.style.backgroundColor = 'lightgrey'
 }
